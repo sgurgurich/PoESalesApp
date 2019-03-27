2d05c09f08fbf0d55c302c2abb7f1ee762f6afc0
sgurgurich <sgurgurich@gmail.com>
2019-03-27 17:08:45 -0400

    linked the FXML to the controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
@@ -12,19 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//BorderPane root = new BorderPane();
 
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
 			
 			Parent root = FXMLLoader.load(getClass().getResource("notifierUi.fxml"));
-
-			//Scene scene = new Scene(root,600,800);
-			
 			primaryStage.setScene(new Scene(root));
 			primaryStage.show();
-			
-			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
