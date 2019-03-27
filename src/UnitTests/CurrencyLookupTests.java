1ac20f8c77352a4ead2d02d0938af2f9a1504255
letmecodeforyou-HF <letmecodeforyou.hf@gmail.com>
2019-03-27 18:34:30 -0400

    fixed some git issues

--- src/UnitTests/CurrencyLookupTests.java
+++ src/UnitTests/CurrencyLookupTests.java
@@ -1,5 +1,9 @@
 package UnitTests;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import utils.CurrencyLookupUtility;
@@ -7,10 +11,41 @@ import utils.CurrencyLookupUtility;
 public class CurrencyLookupTests {
 
 	@Test
-	public void test() {
+	public void successfulConnectTest() {
 		CurrencyLookupUtility clu = new CurrencyLookupUtility();
-		
-		
+		assertTrue(clu.connectToApi());		
+	}
+	
+	@Test
+	public void unsuccessfulConnectTest() {
+		CurrencyLookupUtility clu = new CurrencyLookupUtility();
+		clu.setApiPath("false");
+		assertFalse(clu.connectToApi());		
+	}
+	
+	@Test
+	public void statusGoodBelowRangeTest() {
+		CurrencyLookupUtility clu = new CurrencyLookupUtility();		
+		assertFalse(clu.verifyStatusGood(103));		
+	}
+	
+	@Test
+	public void statusGoodInRangeTest() {
+		CurrencyLookupUtility clu = new CurrencyLookupUtility();		
+		assertTrue(clu.verifyStatusGood(203));		
+	}
+	
+	@Test
+	public void statusAboveRangeTest() {
+		CurrencyLookupUtility clu = new CurrencyLookupUtility();		
+		assertFalse(clu.verifyStatusGood(403));		
+	}
+	
+
+	@Test
+	public void jsonObjGet() {
+		CurrencyLookupUtility clu = new CurrencyLookupUtility();	
+		assertNotNull(clu.getJsonObj());
 	}
 
 }
--- src/utils/CurrencyLookupUtility.java
+++ src/utils/CurrencyLookupUtility.java
@@ -12,6 +12,9 @@ import com.google.gson.JsonParser;
 public class CurrencyLookupUtility {
 
 	private URL url;
+	private JsonObject jsonObj;	
+	private String apiPath = "https://poe.ninja/api/data/currencyoverview?league=Synthesis&type=Currency";
+	private String userAgentString =  "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2";
 
 	public CurrencyLookupUtility() {
 		connectToApi();
@@ -21,11 +24,11 @@ public class CurrencyLookupUtility {
 		boolean output = false;
 		
 		try {
-			url = new URL("path");
+			url = new URL(apiPath);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
 			con.setRequestProperty("Content-Type", "application/json");
-			con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+			con.setRequestProperty("User-Agent", userAgentString);
 			int status = con.getResponseCode();
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -36,10 +39,10 @@ public class CurrencyLookupUtility {
 			}
 			in.close();
 			
-			if (status >= 200 && status < 300) {
+			if(verifyStatusGood(status)) {
 				System.out.println("Content Valid Response");
 				JsonParser parser = new JsonParser();
-				JsonObject o = parser.parse(content.toString()).getAsJsonObject();
+				jsonObj = parser.parse(content.toString()).getAsJsonObject();
 				
 				//System.out.println(o.toString());
 				output = true;
@@ -54,7 +57,22 @@ public class CurrencyLookupUtility {
 		}
 		
 		return output;
-
+	}
+	
+	public JsonObject getJsonObj() {
+		return jsonObj;
+	}
+	
+	public void setApiPath(String apiPath) {
+		this.apiPath = apiPath;
+	}
+	
+	public boolean verifyStatusGood(int status) {
+		if (status >= 200 && status < 300) {
+			return true;
+		}
+		
+		return false;		
 	}
 
 }
